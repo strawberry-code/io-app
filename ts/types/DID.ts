64803e464bd2029eca190855630ff1556e9892ca
Cristiano Cavo <cristianocavo@icloud.com>
2020-11-26 11:05:27 +0100

    generazione del did non più hardcodato, ma randomico

--- ts/types/DID.ts
+++ ts/types/DID.ts
@@ -73,9 +73,9 @@ export class DID {
   public async generateEthWallet(): Promise<void> {
     let secureRandomSeed: Uint8Array = await generateSecureRandom(64) // FIXME: adesso stiamo usando un seed generato a random, ma per il recupero poi come si fa?
     console.log("secure random seed: " + secureRandomSeed)
-    let hardCodedSecureRandomSeedForDemoPurposes = [125,1,252,123,145,224,108,133,154,148,213,119,167,68,122,39,45,91,251,239,101,175,60,237,71,220,200,245,131,143,89,169,67,156,209,210,196,195,205,152,113,226,21,30,199,176,43,123,213,232,235,38,133,129,195,158,7,130,183,37,12,55,185,212]
-    console.log("hardcoded random seed: " + hardCodedSecureRandomSeedForDemoPurposes)
-    let hdnode = ethers.utils.HDNode.fromSeed(hardCodedSecureRandomSeedForDemoPurposes)
+    //let hardCodedSecureRandomSeedForDemoPurposes = [125,1,252,123,145,224,108,133,154,148,213,119,167,68,122,39,45,91,251,239,101,175,60,237,71,220,200,245,131,143,89,169,67,156,209,210,196,195,205,152,113,226,21,30,199,176,43,123,213,232,235,38,133,129,195,158,7,130,183,37,12,55,185,212]
+    //console.log("hardcoded random seed: " + hardCodedSecureRandomSeedForDemoPurposes)
+    let hdnode = ethers.utils.HDNode.fromSeed(secureRandomSeed)
     this.setDidAddress(`did:ethr:${hdnode.address}`)
     this.setEthAddress(hdnode.address)
     this.setPublicKey(hdnode.publicKey)
