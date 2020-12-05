4441ce3b56779e5bc200a7607bb6b60018ab0b00
despotes <pietromaddera@gmail.com>
2020-12-05 21:38:54 +0100

    Prettiefied SsiWalletReceiveScreen

--- ts/screens/ssi/SsiWalletReceiveScreen.tsx
+++ ts/screens/ssi/SsiWalletReceiveScreen.tsx
@@ -1,37 +1,53 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet, TouchableHighlight, Platform } from "react-native"
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  Platform
+} from "react-native";
 import TopScreenComponent from "../../components/screens/TopScreenComponent";
-import I18n from "../../i18n"
-import variables from '../../theme/variables'
+import I18n from "../../i18n";
+import variables from "../../theme/variables";
 
 const SsiWalletReceiveScreen: React.FC = () => {
-
   return (
-      <TopScreenComponent
+    <TopScreenComponent
       faqCategories={["profile", "privacy", "authentication_SPID"]}
       headerTitle={I18n.t("ssi.title")}
       goBack={true}
-      >
-        <Text style={styles.title}>Ricevi nel Wallet</Text>
-          <View style={{ padding: 30, justifyContent: 'space-around'}}>
-            <Image source={require('../../../img/icons/qr_code_image.png')} style={styles.image} />
-            <Text style={styles.descriptionTitle}>Descrizione</Text>
-            <Text style={styles.descriptionText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac ipsum porttitor, egestas erat in, blandit lorem. </Text>
-            <Text style={styles.descriptionTitle}>Indirizzo</Text>
-            <Text style={styles.addressText}>0x123456789</Text>
-          <TouchableHighlight style={button.container} onPress={() => alert('da definire')} >
-            <Text style={button.text}>{I18n.t('ssi.balanceAndTransaction.receiveButton')}</Text>
-          </TouchableHighlight>
-        </View>
-
-      </TopScreenComponent>
-  )
-}
+    >
+      <Text style={styles.title}>Ricevi nel Wallet</Text>
+      <View style={{ padding: 30, justifyContent: "space-around" }}>
+        <Image
+          source={require("../../../img/icons/qr_code_image.png")}
+          style={styles.image}
+        />
+        <Text style={styles.descriptionTitle}>Descrizione</Text>
+        <Text style={styles.descriptionText}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ac
+          ipsum porttitor, egestas erat in, blandit lorem.{" "}
+        </Text>
+        <Text style={styles.descriptionTitle}>Indirizzo</Text>
+        <Text style={styles.addressText}>0x123456789</Text>
+        <TouchableHighlight
+          style={button.container}
+          onPress={() => alert("da definire")}
+        >
+          <Text style={button.text}>
+            {I18n.t("ssi.balanceAndTransaction.receiveButton")}
+          </Text>
+        </TouchableHighlight>
+      </View>
+    </TopScreenComponent>
+  );
+};
 
 const button = StyleSheet.create({
   container: {
     paddingVertical: 10,
-    width: '100%',
+    width: "100%",
     backgroundColor: variables.brandPrimary,
     borderRadius: 5,
     marginTop: 10
@@ -39,15 +55,15 @@ const button = StyleSheet.create({
   text: {
     fontSize: variables.h4FontSize,
     color: variables.colorWhite,
-    textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'Titillium Web' : 'TitilliumWeb-Regular',
-  },
-})
+    textAlign: "center",
+    fontFamily: Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-Regular"
+  }
+});
 
 const styles = StyleSheet.create({
   title: {
-    fontFamily: Platform.OS === 'ios' ? 'Titillium Web' : 'TitilliumWeb-Bold',
-    fontWeight: Platform.OS === 'ios' ? '600' : 'normal',
+    fontFamily: Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-Bold",
+    fontWeight: Platform.OS === "ios" ? "600" : "normal",
     fontSize: variables.h2FontSize,
     padding: 20,
     backgroundColor: variables.brandPrimary,
@@ -57,23 +73,23 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     marginBottom: 30,
-    alignSelf: 'center'
+    alignSelf: "center"
   },
   descriptionTitle: {
-    fontFamily: Platform.OS === 'ios' ? 'Titillium Web' : 'TitilliumWeb-Bold',
-    fontWeight: Platform.OS === 'ios' ? '600' : 'normal',
+    fontFamily: Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-Bold",
+    fontWeight: Platform.OS === "ios" ? "600" : "normal",
     fontSize: variables.h4FontSize,
     color: variables.brandPrimary,
-    textAlign: 'center'
+    textAlign: "center"
   },
   descriptionText: {
     fontSize: variables.fontSizeBase,
     marginBottom: 10
   },
-  addressText : {
+  addressText: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center"
   }
-})
+});
 
-export default SsiWalletReceiveScreen
+export default SsiWalletReceiveScreen;
