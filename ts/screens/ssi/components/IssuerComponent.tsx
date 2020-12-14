e8bc71666eb7b538a5150d6ff0b7f490bbdcee79
despotes <pietromaddera@gmail.com>
2020-12-14 23:32:33 +0100

    Aggiunto traduzione Issuer e piccole migliorie

--- ts/screens/ssi/components/IssuerComponent.tsx
+++ ts/screens/ssi/components/IssuerComponent.tsx
@@ -5,11 +5,14 @@ import {
   View,
   StyleSheet,
   Platform,
-  TouchableOpacity
+  TouchableOpacity,
+  Linking
 } from "react-native";
 
 import variables from "../../../theme/variables";
 import IconFont from "../../../components/ui/IconFont";
+import I18n from "../../../i18n";
+import { TranslationKeys } from "../../../../locales/locales";
 
 interface Props {
   issuer: {
@@ -25,6 +28,42 @@ const IssuerComponent: React.FC<Props> = ({ issuer }) => {
     else setVisible("none");
   };
 
+  const fieldElement = Object.keys(issuer).map(field => {
+    if (field === "id") return null;
+
+    if (field === "informationUri") {
+      return (
+        <View
+          style={[
+            styles.issuerField,
+            { flexDirection: "row", alignItems: "center" }
+          ]}
+        >
+          <Text style={styles.issuerFieldText}>
+            {I18n.t(`ssi.issuer.fields.${field}` as TranslationKeys)}
+          </Text>
+          <TouchableOpacity
+            style={styles.issuerInfoLink}
+            onPress={() => Linking.openURL(issuer[field])}
+          >
+            <Text style={styles.issuerFieldTextLink}>
+              {I18n.t("ssi.linkText")}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
+    return (
+      <View key={field} style={styles.issuerField}>
+        <Text style={styles.issuerFieldText}>
+          {I18n.t(`ssi.issuer.fields.${field}` as TranslationKeys)}
+        </Text>
+        <Text style={styles.issuerInfo}>{issuer[field]}</Text>
+      </View>
+    );
+  });
+
   return (
     <View style={styles.issuerContainer}>
       <View style={styles.issuerTitleContainer}>
@@ -33,7 +72,11 @@ const IssuerComponent: React.FC<Props> = ({ issuer }) => {
           {Object.keys(issuer).length === 1 ? (
             <IconFont name="io-notice" color={variables.brandDanger} />
           ) : (
-            <Text>{issuer.tradeName}</Text>
+            <Text>
+              {issuer.tradeName.length >= 18
+                ? issuer.tradeName.substr(0, 16) + "..."
+                : issuer.tradeName}
+            </Text>
           )}
         </Text>
         <TouchableOpacity onPress={handleOpen}>
@@ -42,15 +85,9 @@ const IssuerComponent: React.FC<Props> = ({ issuer }) => {
       </View>
       <View style={{ display: visible }}>
         {Object.keys(issuer).length <= 1 && (
-          <Text style={styles.issuerInfo}>Firmatario Sconosciuto</Text>
+          <Text style={styles.issuerInfo}>{I18n.t("ssi.issuer.unknown")}</Text>
         )}
-        {Object.keys(issuer).length > 1 &&
-          Object.keys(issuer).map(field => (
-            <>
-              <Text style={styles.issuerInfo}>{_.startCase(field)}</Text>
-              <Text>{issuer[field]}</Text>
-            </>
-          ))}
+        {Object.keys(issuer).length > 1 && fieldElement}
       </View>
     </View>
   );
@@ -58,6 +95,7 @@ const IssuerComponent: React.FC<Props> = ({ issuer }) => {
 
 const styles = StyleSheet.create({
   issuerContainer: {
+    marginBottom: 5,
     backgroundColor: variables.colorWhite,
     borderRadius: 5,
     padding: 10,
@@ -80,9 +118,33 @@ const styles = StyleSheet.create({
     fontFamily: Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-Bold",
     fontWeight: Platform.OS === "ios" ? "600" : "normal"
   },
-  issuerInfo: {
+  issuerField: {
+    marginBottom: 5
+  },
+  issuerFieldText: {
     fontSize: variables.h5FontSize,
+    fontFamily:
+      Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-SemiBold",
+    fontWeight: Platform.OS === "ios" ? "500" : "normal",
+    color: variables.brandPrimary
+  },
+  issuerFieldTextLink: {
+    fontSize: variables.fontSizeBase,
+    fontFamily:
+      Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-SemiBold",
+    fontWeight: Platform.OS === "ios" ? "500" : "normal",
+    color: variables.colorWhite
+  },
+  issuerInfo: {
+    fontSize: variables.fontSizeBase,
     fontFamily: Platform.OS === "ios" ? "Titillium Web" : "TitilliumWeb-Regular"
+  },
+  issuerInfoLink: {
+    backgroundColor: "#003354",
+    marginLeft: 5,
+    paddingVertical: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5
   }
 });
 
