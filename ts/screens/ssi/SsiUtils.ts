2467f09cebc33b3cc5333605d3079a3967de13e8
Cristiano Cavo <cristianocavo@icloud.com>
2020-12-18 13:47:26 +0100

    utilizzo estensione txt esportazione backup vcs

--- ts/screens/ssi/SsiUtils.ts
+++ ts/screens/ssi/SsiUtils.ts
@@ -2,36 +2,36 @@ import * as RNFS from "react-native-fs";
 import {DidSingleton} from "../../types/DID";
 import VCstore from "./VCstore";
 import Share from "react-native-share";
-import base64 from 'react-native-base64';
+import base64 from "react-native-base64";
 
 const exportCredentials = async () => {
-  let filePath = RNFS.DocumentDirectoryPath + '/' + DidSingleton.getEthAddress() + '.ssikey'
+  let filePath = RNFS.DocumentDirectoryPath + `/` + DidSingleton.getEthAddress() + `.txt`
   let payload = await VCstore.getRawJwts()
-  console.log('payload: ' + payload)
+  console.log(`payload: ` + payload)
 
   try {
-    console.log('[exportCredentials]: scrivo le VCs in un file temporaneo...')
-    await RNFS.writeFile(filePath, base64.encode(payload), 'utf8')
-    console.log('[exportCredentials]: le VCs sono state scritte in: ' + filePath)
+    console.log(`[exportCredentials]: scrivo le VCs in un file temporaneo...`)
+    await RNFS.writeFile(filePath, base64.encode(payload), `utf8`)
+    console.log(`[exportCredentials]: le VCs sono state scritte in: ` + filePath)
   } catch (e) {
     console.log(e)
   }
 
   try {
-    console.log('[exportCredentials]: condivido il file con le VCs tramite la share dell\'OS...')
+    console.log(`[exportCredentials]: condivido il file con le VCs tramite la share dell'OS...`)
     let res = await Share.open({
-      title: 'AAA',
-      //url: `data:text/txt;base64,${base64.encode('pincopallo')}`,
+      title: `AAA`,
+      //url: `data:text/txt;base64,${base64.encode(`pincopallo`)}`,
       url: filePath,
-      //filename:base64.encode('SSIKey'),
-      type: '/' + DidSingleton.getEthAddress() + '.ssikey'
+      filename: `${DidSingleton.getEthAddress()}.txt`,
+      type: `/` + DidSingleton.getEthAddress() + `.txt`
     })
-    console.log('[exportCredentials]: processo di condivisione terminato')
-    console.log('[exportCredentials]: ' + JSON.stringify(res));
+    console.log(`[exportCredentials]: processo di condivisione terminato`)
+    console.log(`[exportCredentials]: ` + JSON.stringify(res));
 
-    console.log('[exportCredentials]: elimino ' + filePath + ' ...')
+    console.log(`[exportCredentials]: elimino ` + filePath + ` ...`)
     await RNFS.unlink(filePath)
-    console.log('[exportCredentials]: file eliminato')
+    console.log(`[exportCredentials]: file eliminato`)
 
   } catch (e) {
     console.log(e)
@@ -39,20 +39,16 @@ const exportCredentials = async () => {
 }
 
 const exportCredentialsAndroid = async () => {
-  
+
   try {
-    const payload = await VCstore.getRawJwts();
-    console.log(
-      "[exportCredentials]: condivido il file con le VCs tramite la share dell'OS..."
-    );
+    const payload = base64.encode(base64.encode(await VCstore.getRawJwts()))
+    console.log(`[exportCredentials]: condivido il file con le VCs tramite la share del'OS...`);
     const res = await Share.open({
-      url: `data:text/plain;base64,${base64.encode(payload)}`,
-      cache: true,
-      type: "text/plain",
-      filename: `backup-${DidSingleton.getEthAddress()}-${new Date()}.ssikey`
+      url: `data:text/plain;base64,${payload}`,
+      filename: `${DidSingleton.getEthAddress()}`
     });
-    console.log("[exportCredentials]: processo di condivisione terminato");
-    console.log("[exportCredentials]: " + JSON.stringify(res));
+    console.log(`[exportCredentials]: processo di condivisione terminato`);
+    console.log(`[exportCredentials]: ` + JSON.stringify(res));
   } catch (e) {
     console.log(e);
   }
