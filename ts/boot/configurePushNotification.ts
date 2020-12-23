987e9b3220eef9f6ff8d66f388654ec14c282e8f
despotes <pietromaddera@gmail.com>
2020-12-23 22:10:22 +0100

    Fix Push in background e migliorata logica (testare su ios)

--- ts/boot/configurePushNotification.ts
+++ ts/boot/configurePushNotification.ts
@@ -1,3 +1,4 @@
+/* eslint-disable functional/immutable-data */
 /**
  * Set the basic PushNotification configuration
  */
@@ -15,14 +16,14 @@ import {
   loadSsiNotifications,
   updateSsiNotifications,
   updateNotificationsInstallationToken,
-  updateNotificationsPendingMessage
+  updateNotificationsPendingMessage,
+  clearSsiNotifications
 } from "../store/actions/notifications";
-import {isDevEnv} from "../utils/environment";
-import {JwtPresentationPayload} from "did-jwt-vc";
-import VCstore from "../screens/ssi/VCstore";
-import notificationStore from "../screens/ssi/notificationStore";
-import {decodeVerifiablePresentation} from "../screens/ssi/VerifiablePresentations";
-import {navigateToVCsList, navigateToSsiNotificationScreen} from "../store/actions/navigation";
+import { isDevEnv } from "../utils/environment";
+import {  navigateToSsiNotificationScreen } from "../store/actions/navigation";
+import { navigationCurrentRouteSelector } from "../store/reducers/navigation";
+import ROUTES from "../navigation/routes";
+
 
 /**
  * Helper type used to validate the notification payload.
@@ -35,6 +36,81 @@ const NotificationPayload = t.partial({
   })
 });
 
+const handleNotification = (notification) => {
+  console.log('🎈 notifica push arrivata! ('+Platform.OS+')');
+  console.log('dettagli della push: ' + JSON.stringify(notification));
+
+  if (debugRemotePushNotification) {
+    Alert.alert("Notification", JSON.stringify(notification));
+  }
+
+  let ssiNotificationPayload
+
+  if(Platform.OS === 'ios') {
+    ssiNotificationPayload = notification.data.payload
+  } else {
+    ssiNotificationPayload = JSON.parse(notification.payload)
+  }
+
+  console.log('ssiNotificationPayload: ' + JSON.stringify(ssiNotificationPayload))
+
+  let ssiPushType = ssiNotificationPayload.type
+
+  if(ssiPushType === 'ssi-issuedVC') {
+    console.log('sto gestendo una notifica push SSI del tipo: ', ssiPushType);
+    handleIssuedVCNotification(ssiNotificationPayload.verifiablePresentation, notification.foreground);
+  } else {
+    console.log(`⚠️ attenzione: il tipo di notifica push SSI (${ssiPushType}) non è ancora gestito`);
+  }
+
+  const maybeMessageId = fromEither(
+    NotificationPayload.decode(notification)
+  ).chain(payload =>
+    fromNullable(payload.message_id).alt(
+      fromNullable(payload.data).mapNullable(_ => _.message_id)
+    )
+  );
+
+  maybeMessageId.map(messageId => {
+    // We just received a push notification about a new message
+    if (notification.foreground) {
+      // The App is in foreground so just refresh the messages list
+      store.dispatch(loadMessages.request());
+    } else {
+      // The App was closed/in background and has been now opened clicking
+      // on the push notification.
+      // Save the message id of the notification in the store so the App can
+      // navigate to the message detail screen as soon as possible (if
+      // needed after the user login/insert the unlock code)
+      store.dispatch(
+        updateNotificationsPendingMessage(
+          messageId,
+          notification.foreground
+        )
+      );
+
+      // finally, refresh the message list to start loading the content of
+      // the new message
+      store.dispatch(loadMessages.request());
+    }
+  });
+
+  // On iOS we need to call this when the remote notification handling is complete
+  notification.finish(PushNotificationIOS.FetchResult.NoData);
+};
+
+const handleRegister = (device) => {
+  console.log('PUSH NOTIFICATIONS TOKEN: ', device.token);
+  // Dispatch an action to save the token in the store
+  store.dispatch(updateNotificationsInstallationToken(device.token));
+  PushNotification.popInitialNotification((notification) => {
+    if (notification) {
+      handleNotification(notification);
+    }
+  });
+};
+
+
 function configurePushNotifications() {
   // if isDevEnv, disable push notification to avoid crash for missing firebase settings
   // Cristiano: disabilitato perchè è necessario avere le push abilitate anche in dev mode
@@ -43,108 +119,30 @@ function configurePushNotifications() {
   // }
   PushNotification.configure({
     // Called when token is generated
-    onRegister: async token => {
-      // Dispatch an action to save the token in the store
-      console.log('PUSH NOTIFICATIONS TOKEN: ' + token.token)
-      await AsyncStorage.setItem('PUSH_TOKEN', token.token)
-      store.dispatch(updateNotificationsInstallationToken(token.token));
+    onRegister: device => {
+      handleRegister(device);
     },
 
     // Called when a remote or local notification is opened or received
     onNotification: notification => {
-
-      console.log('🎈 notifica push arrivata! (' + Platform.OS + ')')
-      console.log('dettagli della push: ' + JSON.stringify(notification))
-
-      if (debugRemotePushNotification) {
-        Alert.alert("Notification", JSON.stringify(notification));
-      }
-
-      let ssiNotificationPayload
-
-      if (Platform.OS === 'ios') {
-        ssiNotificationPayload = notification.data.payload
-      } else {
-        ssiNotificationPayload = JSON.parse(notification.payload)
-      }
-
-      console.log('ssiNotificationPayload: ' + JSON.stringify(ssiNotificationPayload))
-
-      let ssiPushType = ssiNotificationPayload.type
-
-      if (ssiPushType === 'ssi-issuedVC') {
-        console.log('sto gestendo una notifica push SSI del tipo: ' + ssiPushType);
-        handleIssuedVCNotification(ssiNotificationPayload.verifiablePresentation, notification.foreground);
-      } else {
-        console.log('⚠️ attenzione: il tipo di notifica push SSI (' + ssiPushType + ') non è ancora gestito')
-      }
-
-      const maybeMessageId = fromEither(
-        NotificationPayload.decode(notification)
-      ).chain(payload =>
-        fromNullable(payload.message_id).alt(
-          fromNullable(payload.data).mapNullable(_ => _.message_id)
-        )
-      );
-
-      // TODO MANCA DA GESTIRE LE LOGICHE PUSH CUSTOM SSI
-      // if(notification.foreground) {
-      //   console.log('unimplemented')
-      // } else {
-      //   AsyncStorage.getItem('notifications').then(notifications => {
-      //     if(notifications === null) {
-      //       notifications = []
-      //     } else {
-      //       notifications = JSON.parse(notifications)
-      //       console.log(notifications)
-      //     }
-      //   })
-      // }
-      // TODO MANCA DA GESTIRE LE LOGICHE PUSH CUSTOM SSI
-
-      maybeMessageId.map(messageId => {
-        // We just received a push notification about a new message
-        if (notification.foreground) {
-          // The App is in foreground so just refresh the messages list
-          store.dispatch(loadMessages.request());
-        } else {
-          // The App was closed/in background and has been now opened clicking
-          // on the push notification.
-          // Save the message id of the notification in the store so the App can
-          // navigate to the message detail screen as soon as possible (if
-          // needed after the user login/insert the unlock code)
-          store.dispatch(
-            updateNotificationsPendingMessage(
-              messageId,
-              notification.foreground
-            )
-          );
-
-          // finally, refresh the message list to start loading the content of
-          // the new message
-          store.dispatch(loadMessages.request());
-        }
-      });
-
-      // On iOS we need to call this when the remote notification handling is complete
-      notification.finish(PushNotificationIOS.FetchResult.NoData);
+      handleNotification(notification);
     },
+    popInitialNotification: false,
 
     // GCM Sender ID
-    // BEFORE
-    // senderID: gcmSenderId
-    // AFTER
     senderID: gcmSenderId
   });
 }
 
-let guard = false
-
 function handleIssuedVCNotification(VPjwt: string, foreground: boolean) {
 
-
-  if (!guard) {
-    guard = true
+  store.dispatch(updateSsiNotifications(VPjwt));
+  const currentRoute = navigationCurrentRouteSelector(store.getState());
+  // CONTROLLO CHE L'ATTUALE ROUTE SIA QUELLA DELLE NOTIFICATIONS
+  console.log("CURRENT NAV", currentRoute);
+ 
+  // SE IN FOREGROUND FACCIO VEDERE UN ALLERT DI NOTIFICA CHE CHIEDE SE VUOLE NAVIGARE NELLA VISTA DEL NOTIFICHE
+  if (foreground && currentRoute.value !== ROUTES.SSI_NOTIFICATIONS) {
     Alert.alert(
       'Nuova notifica',
       'Hai ricevuto una richiesta di accettazione per la credenziale firmata',
@@ -152,30 +150,24 @@ function handleIssuedVCNotification(VPjwt: string, foreground: boolean) {
         {
           text: 'Notifiche',
           onPress: () => {
-            guard = false
-            if (foreground) {
-              store.dispatch(updateSsiNotifications(VPjwt));
-              PushNotification.cancelAllLocalNotifications();
               store.dispatch(navigateToSsiNotificationScreen());
-              console.log('salvando push SSI nel redux persist store');
-            } else {
-              // FIXME: LE NOTIFICHE SU ANDROID NON VENGONO SALVATE NEL NOTIFICATION CENTRE QUANDO IN BACKGROUND
-              store.dispatch(updateSsiNotifications(VPjwt));
-              store.dispatch(navigateToSsiNotificationScreen());
-            }
-            return true;
+           return true;
           }
         },
         {
           text: 'Chiudi',
           style: 'destructive',
-          onPress: () => {guard = false}
+          onPress: undefined
         }
       ],
-      {cancelable: false}
+      { cancelable: true }
     );
-
   }
-}
+
+  if (!foreground){
+    store.dispatch(navigateToSsiNotificationScreen());
+  }
+
+ }
 
 export default configurePushNotifications;
